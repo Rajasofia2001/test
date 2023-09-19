cc05722778eed5686ab0e2010e1d452dba8161cc
Rajasofia2001 <rajasofia1011@gmail.com>
2023-09-19 18:15:57 +0530

    first commit

---
+++ B.java
@@ -0,0 +1,9 @@
+class B 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("i am into test lara");
+		System.out.println("i am into test lara");
+		System.out.println("i am into test lara");
+	}
+}
